67588ef9b6b4eddcc78d6b8d8d858679615d64f6
Norgeth <111367440+Norgeth@users.noreply.github.com>
2022-10-28 18:13:26 +0200

    up

--- main.js
+++ main.js
@@ -1,15 +1,54 @@
-// http://websamuraj.pl/examples/js/projekt11/
-
 const btnStart = document.querySelector('button.start');
 const btnReset = document.querySelector('button.reset');
 const divTime = document.querySelector('div.insideTime');
-const nowTime = new Date().getTime();
 
-btnStart.addEventListener('click', () => {
-    setInterval(()=>{
-        
-        let seconds = (new Date().getTime() / 1000) - (nowTime / 1000)
-        divTime.textContent = seconds.toFixed(2)
-    },10)
+let active = false;
+let myInterval;
+
+const startStoper = ()=>{
+
+ active = !active;
+ 
+  if(active){
+
+   const clickTime = new Date().getTime() / 1000;
     
-})
+   const stoper = ()=>{
+
+    const startTime = new Date().getTime() / 1000;
+    let time = startTime - clickTime;
+    const stoperTime = time;
+    divTime.textContent = stoperTime.toFixed(2);
+    console.log("elo");
+
+    }
+     myInterval = setInterval(stoper, 10);
+     btnStart.textContent = "Pauza";
+     console.log("Start");
+
+} 
+
+  else if (!active){
+
+   btnStart.textContent = "Start";
+   clearInterval(myInterval);
+   console.log("Pauza");
+
+}
+
+
+
+}
+
+const reset = ()=>{
+
+ divTime.textContent = "---";
+ clearInterval(myInterval);
+ active = false;
+ btnStart.textContent = "Start";
+ console.log("Reset");
+
+}
+
+btnStart.addEventListener('click', startStoper);
+btnReset.addEventListener('click', reset);
